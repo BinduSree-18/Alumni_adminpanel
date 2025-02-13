5fff4a13996679541727fef5ad3afd5cded24be6
Yeluri Nakshatra <nakshatra.yeluri@gmail.com>
2025-02-13 15:50:20 +0530

    asdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Layout from './Components/Layout'
+import Upload from './uploadimage/Upload';
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 const App = () => {
   return (
    <Router>
     <Routes>
+      <Route path="/upload" element={<Upload/>} />
       <Route path="/" element={<Layout />} />
       <Route path="*" element={<Layout />} />
     </Routes>
